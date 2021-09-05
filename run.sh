cd9472e92c878ad594b1fdd33d71247798ca75ca
Michael Li <er.li@mail.mcgill.ca>
2021-09-05 01:17:27 -0400

    CC job submission script

---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --gres=gpu:2              # Number of GPU(s) per node
+#SBATCH --cpus-per-task=24        # CPU cores/threads
+#SBATCH --mem=128G                # memory
+#SBATCH --time=3-0                # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
+#SBATCH --job-name=cl         
+#SBATCH --output=logs/%j
+#SBATCH --mail-user=er.li@mail.mcgill.ca
+#SBATCH --mail-type=ALL
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+# confirm gpu available
+nvidia-smi
+
+# activate env
+source env/bin/activate
+
+# validation python version
+python --version
+
+python examples/recon/continual_learning.py -eid cl > continual_learning.log 2>&1 &
