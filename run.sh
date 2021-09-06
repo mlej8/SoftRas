6e389f96139a9bbebe6f989febff65621c55453d
Michael Li <er.li@mail.mcgill.ca>
2021-09-06 10:52:54 -0400

    CC settings for Graham

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --gres=gpu:2              # Number of GPU(s) per node
-#SBATCH --cpus-per-task=24        # CPU cores/threads
+#SBATCH --gres=gpu:v100l:1        # Number of GPU(s) per node
+#SBATCH --cpus-per-task=8         # CPU cores/threads
 #SBATCH --mem=128G                # memory
 #SBATCH --time=3-0                # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
 #SBATCH --job-name=cl         
@@ -19,4 +19,4 @@ source env/bin/activate
 # validation python version
 python --version
 
-python examples/recon/continual_learning.py -eid cl > continual_learning.log 2>&1 &
+python examples/recon/continual_learning.py -eid cl --consolidate > continual_learning_consolidate.log 2>&1
