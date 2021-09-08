d53aa9d479a23549dcb915bf0743c16d33d47f88
Michael Li <er.li@mail.mcgill.ca>
2021-09-08 09:58:00 -0400

    CC: Interactive job example

--- interactive.sh
+++ interactive.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-#SBATCH --gres=gpu:v100l:1        # Number of GPU(s) per node
+#SBATCH --gres=gpu:v100:1        # Number of GPU(s) per node
 #SBATCH --time=0:15:00                # A time limit of zero requests that no time limit be imposed. Acceptable time formats include "minutes", "minutes:seconds", "hours:minutes:seconds", "days-hours", "days-hours:minutes" and "days-hours:minutes:seconds".
 #SBATCH --nodelist=gra1337
+#SBATCH --mem=16G   
 
-salloc --time=0:15:00  --nodelist=gra1337 --gres=gpu:v100l:1
+salloc --time=0:15:00  --nodelist=gra1337 --gres=gpu:v100:1 --mem=16G   
