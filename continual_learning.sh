736660a3b2c7f0f1687ee69abcf78ad17bf94483
Er Jun Li <er.li@mail.mcgill.ca>
2021-09-13 14:59:26 -0400

    Launch script for local machine

--- continual_learning.sh
+++ continual_learning.sh
@@ -1,5 +1,8 @@
-#! /bin/sh
+#! /bin/bash
 
 CUDA_VISIBLE_DEVICES=0
 
-nohup python examples/recon/continual_learning.py -eid cl > continual_learning.log 2>&1 &
+python examples/recon/continual_learning.py -eid cl \ 
+    --model-directory data/results/models \ 
+    --dataset-directory /mnt/e/Data/mesh_reconstruction \ 
+    --consolidate > continual_learning.log 2>&1 &
