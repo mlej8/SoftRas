cd51984eab798ea1b6ef0b525f43054f6f27538c
Er Jun Li <er.li@mail.mcgill.ca>
2021-10-03 17:55:12 -0400

    Use HDD for checkpoints

--- continual_learning_consolidate.sh
+++ continual_learning_consolidate.sh
@@ -3,6 +3,6 @@
 CUDA_VISIBLE_DEVICES=0
 
 python examples/recon/continual_learning.py -eid cl \
-    -md data/results/models \
+    -md "/mnt/e/Continual Learning/results/models" \
     -dd /mnt/e/Data/mesh_reconstruction \
     --consolidate > continual_learning_consolidate.log 2>&1 &
